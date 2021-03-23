b3e42a246a92fc6cfd89316b9a7e7db40383d81b
Tyler Noblett <tyler.noblett@outlook.com>
2021-03-23 18:55:01 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# beatlesai
+# beatles.ai-code
+This repo contains the code that generated the music behind https://www.beatlesai.com/.
+
+Note: This isn't code that can be run as repo, but instead shows the basic python code I used to train the two models, and the javascript code I used to convert the JSON representation of the music into [ABC](https://abcnotation.com/) format.
+
+## What's in this repo?
+`beatles-full-second-iter.ipynb` shows how I went about training the two neural nets (one for lyrics and the other for 'music').
+
+`converter.js` converts the JSON music into ABC Notation
+
+`data/i-feel-fine.abc` contains an example of a few lines of "I feel fine" in ABC notation
+`data/yesterday-lyrics` contains the lyrics in the format I used to train the neural net
+`data/yesterday-initial` contains the initial format of the music (stripped of key down to relative chords and melody) I manually typed out from sheet music.
+`data/yesterday-json` contains a json version of the music (stripped of key down to relative chords and melody) so that the neural net would return the 
+
+## Why?
+In February of 2020 I completed the first fast.ai v1 course, and wanted to create a vision and an NLP project to apply what I learned. I wanted to do something creative, and while I found a lot of NN based projects regarding music, I couldn't find anything that dealt specifically with writing songs (ie chord progressions, melody, and lyrics), so being a Beatles fan, as well as the fact that they have a deep catalogue (which helps with lyric generation) and since most of their music is avaiable in sheet music notation (which I needed to create the custom musical encoding that I used to train the model), I chose to use their music.
+
+## How does it work?
+I created two fast.ai text learner models; one for lyrics and the other for the actual 'music'. The musical encoding is cleaned up by a few helper functions (I think if I had more data to work with, this wouldn't have been necessary).
+The lyrics are then fed into the musical encoding, which is converted to JSON. That JSON is parsed, and is fed into another utility that converts the JSON encoding into an ABC string (which is a kind of musical notation mostly used by folk musicians). Converting to the notation into ABC was essential because ABCJS was the only library I could find that rendered music and played a midi file (with accompaniment) in the browser, which was crucial to make this project something that could be appreciated.
+
+## Why use a custom encoding?
+What I really wanted to do was create a model that was key agnostic, meaning that instead of learning music by specific notes or chords it learned their relative values. This would make it easier to train a model to make something that sounded more like real music becuase it isn't trying to combine multilpe keys into one. 
+
+![sheet music](holdyourhand.png)
+Here's the first line of the verse in `I Want To Hold Your Hand`, and here's the encoding. 
+
+HOW TO READ THIS?
+```json
+["Imaj", "w", 7, 0.75], ["Imaj", "c", 5, 0.25], ["Imaj", "c", 4, 1], ["Imaj", "n", null, 1], ["Imaj", "w", 4, 0.5], ["Imaj", "w", 7, 0.5],
+```
+The arrays signify each new note that is sung. The first value is the chord relative to the key (which is not quite in nashville numbering because of issues with capitalization). 
+
+The second item displays whether or not the new note is a word ("w"), a continutation of the word but with a different note sung ("c"), or a rest ("n"). 
+
+The third value is the value of the note relative to the key; for example, a `7` indicates that the note is seven half tones above the base note (if the song is in the key of `C` the value of `7` would translate to `G`). The value is  `null` if the note is a rest.
+
+The final value is the lenght of the note relative to a quarter note; `0.75` is a dotted eighth note, `0.5` is an eighth note etc.
+
+## Creating the Data 
+Transcribing a song to my data format intially took about 75 minutes per song, but by the end I could transcribe a song in about half of the time. The songs were mostly taken from the Beatles collection called #1
+
+## Issues
+Because creating the data took so much time, I wasn't able to train the model as well as I would like. Because of this, I had to really help it out - it wasn't picking up on breaking the song into specific sections (verse, chorus, etc) so I gave it a lot of help - in that sense, I did the heavy structural lifting while the because the learner couldn't determine separate portions very well with the limited data it had, which is huge bummer! 
+
+### nashville numbering system
+My inspiration is that as a musician, a lot of the gigs I've played involve the nashville numbering system. I won't go into all of it here, but the beauty of the nashville numbering system is that it doesn't matter what key the song is originally in, as long as you know the key, you can read the numerical chord numbers and deduce what chords you should play. For instance, the chords to the first line of "Don't Stop Believing'" are `E B C#m A` (the key being `E`), but if you want to easily play it in another key, you can transpose the chords to `I V vi IV`, and as long as you know the key the song is in, you can easily know waht chords to play. 
+
+My thought was that stripping the music down to these basic roman numeral chords would help the model identify common chord structure; wheras if you trained it on songs that hadn't been broken down into the roman numerals would get something that sounded more like gibberish; ironically, my model still doesn't sound too much like real music, but it could've sounded much worse!
+
+### Melody
+That's all fine and well for chords, but how to handle the vocal lines? In a simliar way, I broke down the melody where the note the key was in into halftones (imitating the frets on a guitar). For instance, in the key of `G`, `G` is `0`, `D` is `7`, and `F` is 10.
+
+### Converting to abcjs
+`converter.js` takes the melody number, chord symbol and rythmic number and converts them into ABC notation in they key of G and adds them to a string.
+
+### explain issues 
+This isn't a perfect system - for example, in most cases chords only change once or twice a measure, but because of the importance of linking the note sung to a chord the model might learn to change chords more quickly. However, this wasn't too big of an issue.
+
+## What I learned:
+My main takeaway was that I needed way more data.
+
+However, training the model on so few songs meant that I ran into a few issues. 
+1. The main issue is that while the network learned the encoding and could create music, it had a harder time picking song structure, so most of the songs would just be a verse that was really long, with some occasionally creating a short bridge or chorus. I think with enough data this problem could be remedied. 
+
+2. Additionally, my hope was to encode the lyrics within the music; it became apparent to me early on that including lyrics within the model only confused it, and 99% of the lyrics the unified netowork generated were 'love', 'you' and 'I'. As you can tell, the lyrics still don't make a lot of sense when a network is only trained on lyrics, so encoding the two together doesn't seem like a realistic goal right now given the technology, even if I had all of the beatles music encoded in that format.
---
+++ converter.js
@@ -0,0 +1,203 @@
+const json = `{"tempo": 112, "verse_one": [["vi", " c ", 2, 1], ["I", "you", 2, 1], ["vi", " c ", -1, -5], ["I7", " n ", null, 1.5], ["vi", "re", 0, 1], ["I", " c ", 4, 1], ["IV", "lied", 16, 0.5], ["I", " c ", 5, 0.5], ["I7", " c ", 7, 0.25], ["IV", "to", 5, 0.5], ["vi", "Ted", 7, 0.5], ["I", "used", 7, 1], ["I", " c ", 7, 0.5], ["IV", "to", 16, 0.5], ["I", "get", 4, 0.5], ["vi", " c ", 7, 0.5], ["I", "her", 12, 0.5], ["I", "back", 9, 1], ["II", "by", 5, 0.5], ["VII", " c ", 2, 1], ["V", "the", 12, 0.5], ["I", " c ", 17, 0.5], ["vi", " c ", 10, 0.5], ["vi", "bottom", 12, 0.5], ["IV", " c ", 10, 0.5], ["V7", "to", 4, 2.5], ["I", "find", 4, 0.25], ["vi", "the", 9, 0.5], ["I", "mine", 7, 0.5], ["vi", " c ", 7, 2], ["V7", "But", 9, 0.5], ["V", "he", 17, 0.5], ["VII", "did", 14, 0.5], ["vi", "nt", 3, 1.5], ["I", "know", 4, 0.5], ["IV", " c ", 5, 0.5], ["vi", "how", 12, 0.5], ["vi7", "to", 4, 1], ["I", "run", 4, 1], ["I", "it", 4, 0.5], ["IV", "by", 4, 0.5], ["vi", "the", 2, 0.5], ["I", "end", 5, 1], ["V", "of", 5, 0.5], ["V", "the", 4, 0.5], ["V7", "book", 7, 0.5], ["IV", "He", 7, 0.5], ["I", "said", 4, 1], ["V", ":", 2, 0.25]], "verse_two": [["vi", " c ", 2, 1], ["I", "when", 2, 1], ["vi", " c ", -1, -5], ["I7", " n ", null, 1.5], ["vi", "Information", 0, 1], ["I", " c ", 4, 1], ["IV", "the", 16, 0.5], ["I", " c ", 5, 0.5], ["I7", " c ", 7, 0.25], ["IV", "show", 5, 0.5], ["vi", "was", 7, 0.5], ["I", "gone", 7, 1], ["I", " c ", 7, 0.5], ["IV", "More", 16, 0.5], ["I", "than", 4, 0.5], ["vi", " c ", 7, 0.5], ["I", "the", 12, 0.5], ["I", "children", 9, 1], ["II", "in", 5, 0.5], ["VII", " c ", 2, 1], ["V", "life", 12, 0.5], ["I", " c ", 17, 0.5], ["vi", " c ", 10, 0.5], ["vi", "at", 12, 0.5], ["IV", " c ", 10, 0.5], ["V7", "the", 4, 2.5], ["I", "party", 4, 0.25], ["vi", "The", 9, 0.5], ["I", "Free", 7, 0.5], ["vi", " c ", 7, 2], ["V7", "Tonight", 9, 0.5], ["V", "and", 17, 0.5], ["VII", "just", 14, 0.5], ["vi", "beside", 3, 1.5], ["I", "him", 4, 0.5], ["IV", " c ", 5, 0.5], ["vi", "In", 12, 0.5], ["vi7", "the", 4, 1], ["I", "evening", 4, 1], ["I", "Sixty", 4, 0.5], ["IV", "-", 4, 0.5], ["vi", "four", 2, 0.5], ["I", "And", 5, 1], ["V", "three", 5, 0.5], ["V", "four", 4, 0.5], ["V7", "at", 7, 0.5], ["IV", "day", 7, 0.5], ["I", "a", 4, 1], ["V", "shell", 2, 0.25]], "chorus": [["V7", "i", 2, 1], ["I", "m", -1, 1], ["I", "feeling", 7, 0.5], ["IV", "up", 0, 1], ["IV", "for", 7, 1], ["IV", "the", 4, 0.5], ["vi", "One", 7, 0.5], ["I7", "Ma", 7, 0.5], ["I", "Ma", 12, 0.5], ["IV", "ma", 4, 0.5], ["I", "ma", 5, 0.5], ["I", "Ma", 2, 0.25], ["vi", "ma", 9, 1], ["I", "Ma", 7, 0.5], ["I", "ma", 7, 1], ["IV", "ma", 7, 0.5], ["vi", "ma", 5, 0.5], ["I", "ma", 4, 0.5], ["I", " c ", 7, 1], ["I", "ma", 4, 0.5], ["I", " c ", 9, 1], ["I", "ma", 7, 0.5], ["I", " c ", 4, 2], ["IV", " c ", 3, 2], ["I", "Ma", 12, 0.5], ["vi", " n ", null, 0.5], ["I", " c ", 7, 1], ["V7", "ma", 4, 0.5], ["I", "Ma", 4, 0.5], ["V7", "ma", 12, 0.5], ["I", " c ", 4, 0.75], ["I", "ma", 7, 1], ["IV", " c ", 5, 1], ["I", " 12 ", 3, 0.5], ["VII", "Ma", 4, 0.5], ["IV", " n ", null, 4], ["I", "ma", 7, 1], ["I", "Ma", 7, 0.5], ["V7", "ma", 2, 1], ["bVI", "ma", 12, 1], ["V7", " c ", 7, 1], ["I", "ma", 11, 1]], "bridge": [["IV", " c ", 9, 3], ["IV", " n ", null, 1.5], ["V", " c ", 2, 0.5], ["I", "cant", 12, 0.5], ["I", "she", 7, 0.5], ["IV", "shake", 12, 0.5], ["I", " c ", 5, 2], ["IV", "in", 12, 2], ["I", "the", 2, 1], ["V7", "dark", 4, 0.5], ["viDim", " c ", 7, 1], ["I", "She", 12, 0.5], ["I", "s", 10, 1], ["I", "got", 7, 0.25], ["I", "a", 4, 1], ["I", "guitar", 4, 0.5], ["V", "to", 5, 0.5], ["I", " c ", 4, 0.5], ["I", "tell", 11, 0.5], ["I7", "her", 2, 2], ["vi", "that", 5, 0.5], ["IV", "she", 7, 0.5], ["IV", "will", 0, 0.5], ["IV", "be", 4, 0.5], ["I", "too", 4, 0.5], ["I", "rich", 0, 0.5], ["IV", " c ", 2, 1], ["IV", " c ", 10, 0.5], ["I", "in", 7, 0.5], ["I", " c ", 2, 0.5], ["vi", "the", 11, 1], ["I", "universe", 10, 0.5], ["I", " c ", 9, 1], ["vi", "She", 7, 0.5], ["I7", " n ", null, 0.5]]}`;
+const musicObj = JSON.parse(json);
+const verseOne = musicObj.verse_one;
+const verseTwo = musicObj.verse_two;
+const chorus = musicObj.chorus;
+const bridge = musicObj.bridge;
+
+
+let abcString = `T:New Beatles Song\nM: 4/4\nL: 1/4\nQ: ${musicObj.tempo}\nK: G\n`;
+// TODO: maybe if it's mobile just print one bar instead of two?
+
+const toneMapping = {
+  "-7": "C",
+  "-6": "^C",
+  "-5": "D",
+  "-4": "_E",
+  "-3": "E",
+  "-2": "=F",
+  "-1": "F", 
+  "0": "G",
+  "1": "^G",
+  "2": "A",
+  "3": "_B",
+  "4": "B",
+  "5": "c",
+  "6": "^c",
+  "7": "d",
+  "8": "_e",
+  "9": "e",
+  "10": "=f",
+  "11": "f", 
+  "12": "g",
+  "13": "^g",
+  "14": "a",
+  "15": "^a",
+  "16": "b",
+}
+
+const chordMapping = {
+  "i": "Gm",
+  "I": "G",
+  "I6": "G6",
+  "I7": "G7",
+  "Isus": "Gsus",
+  "I/vi": "G/Em",
+  "I/vii": "G/F#",
+  "I/V": "G/D",
+  "Imaj7": "Gmaj7",
+  "ii": "Am",
+  "ii7": "Am7",
+  "II": "A",
+  "II7": "A7",
+  "bIII": "Bb",
+  "iii": "Bmin",
+  "iii7": "Bmin7",
+  "III": "B",
+  "III7": "B7",
+  "iv": "Cm",
+  "iv7": "Cm7",
+  "IV": "C",
+  "IV7": "C7",
+  "IV/V": "C/D",
+  "v": "Dmin",
+  "V": "D",
+  "V7": "D7",
+  "Vsus": "Dsus",
+  "V/I": "D/G",
+  "bVI": "Eb",
+  "vi": "Em",
+  "vi6": "Em6",
+  "vi7": "Em7",
+  "vi/V": "Em/D",
+  "vi/VII": "Em/#F",
+  "VI": "E",
+  "VI7": "E7",
+  "vidim": "Edim",
+  "VII": "F",
+  "VIImaj7": "Fmaj7",
+  "vii": "F#dim",
+}
+
+const lengthMapping = {
+  0.125: '/8',
+  0.17: '/6',
+  0.25: '/4',
+  0.33: '1/3',
+  0.5: '/2',
+  0.66: '2/3',
+  0.75: '3/4',
+  1: '',
+  1.25: '5/4',
+  1.33: '4/3',
+  1.5: '3/2',
+  1.66: '5/3',
+  1.75: '7/4',
+  2: '2',
+  2.25: '9/4',
+  2.33: '7/3',
+  2.5: '5/2',
+  2.66: '8/3',
+  2.75: '11/4',
+  3: '3',
+  3.25: '13/4',
+  3.33: '10/3',
+  3.5: '7/2',
+  3.66: '11/3',
+  3.75: '15/4',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
+}
+
+
+const strIsContinuation = (str) => str === ' c ';
+const strIsRest = (str) => str === ' n ';
+const createABCData = (songSegment) => {
+  let bar = 0;
+  let music = '';
+  let lyrics = '';
+  let previousChord = '';
+  let previousBar = 0;
+  let continueTone = false;
+  // TODO: how to handle continutations?
+  // chagne this to for / in loop?
+  // Do I need to access i?
+  // or even a forEach/Map?
+  for (i = 0; i < songSegment.length; i++){
+    const grouping = songSegment[i];
+       
+    //TODO: move this function out of createABCData?
+    const breakMusicApart = (grouping) => {      
+      // TODO: move this out of breakMusicApart?
+      // Move vars into this instead of taking as args?
+      const handleGrouping = (grouping) => {
+        const chord = chordMapping[grouping[0]];
+        const toneOrRest = toneMapping[grouping[2]];
+        let tone = toneOrRest ? toneOrRest : 'z';
+        tone = continueTone ? "-" + tone : tone;
+        let lyric = grouping[1];
+        const length = +grouping[3].toFixed(2);
+        const abcLength = !lengthMapping[length] && length === 1 ? lengthMapping[length] : '/2';
+        previousBar = bar;
+        bar += length
+        continueTone = false
+
+        if (strIsContinuation(lyric) || strIsRest(lyric)){
+          lyric = '* ';
+        }
+        lyrics += `${lyric} `;
+
+        if (previousChord !== chord) {
+          music += `"${chord}"`;
+          previousChord = chord;
+        }
+        music += `${tone + abcLength}`;
+
+        if(bar === 4){
+          abcString += `${music}|\nw: ${lyrics}\n` 
+          bar = 0;
+          music = '';
+          lyrics = '';
+        } 
+      }
+      
+      if (bar + grouping[3] <= 4) {
+         handleGrouping(grouping)
+        // TODO: separte bit where bar === 4 into own thing?
+      } else {
+        const endOfLineGrouping = grouping;
+        const originalLength = grouping[3];
+        // length of bar before it was given this length and minus 4
+        // so that the bar === 4
+        endOfLineGrouping[3] = 4 - bar
+        handleGrouping(endOfLineGrouping, true)
+
+        continueTone = true
+        newGrouping = grouping;
+        newGrouping[1] = " c ";
+        newGrouping[2] = grouping[2];
+        // newGrouping[3] = 4 - originalLength;
+        newGrouping[3] = originalLength - endOfLineGrouping[3];
+        // TODO: maybe? reset previousChord to '' so Chords get printed again?
+        // call function on this grouping before loop continues
+        breakMusicApart(newGrouping);
+      }
+    }
+    breakMusicApart(grouping);
+  }
+}
+// Create the verses together - two lines of lyrics
+// put the chorus at the end and repeat it
+// bridge
+// chorus (maybe use a coda?)
+createABCData(verseOne)
+createABCData(chorus)
+createABCData(verseTwo)
+createABCData(chorus)
+createABCData(bridge)
+createABCData(chorus)
+
+console.log(abcString)
